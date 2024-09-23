2a1df2461e9676f6ee7a610edc9319538a8b52bf
habibmir808 <habibmir808@gmail.com>
2024-09-24 00:51:08 +0600

    change footer background color and structure search page code

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -6,65 +6,76 @@ import Container from "@/components/shared/container";
 import "@/styles/components/pages/search.scss";
 import qs from "qs";
 import { getStrapiData } from "@/utils";
+import { notFound } from "next/navigation";
 
-async function page({
-  searchParams,
-}: {
-  searchParams: {
-    con?: string;
-    des?: string;
-    q?: string;
-    cat?: string;
-    page?: number;
-  };
-}) {
-  const { con, des, q, cat, page } = searchParams;
-
-  let filter: object[] = [];
-  if (q) {
-    filter.push({ title: { $containsi: q } });
-  }
-
-  if (con) {
-    filter.push({ continent: { slug: { $eq: con } } });
-  }
-  if (des) {
-    filter.push({ destination: { slug: { $eq: des } } });
-  }
-
-  if (cat) {
-    filter.push({ categories: { slug: { $eq: cat } } });
-  }
-
-  const query = qs.stringify({
-    populate: ["images.landscape"],
-    fields: ["title", "slug", "description", "createdAt", "readTime"],
-    filters: {
-      $and: filter,
-    },
-    pagination: {
-      page: page || 1,
-      pageSize: 5,
-    },
-  });
-
-  const blogs = await getStrapiData("blogs", query);
-
-  return (
-    <div className="search">
-      <Navbar />
-      <Container maxWidth="lg">
-        <Banner
-          resultCount={blogs.meta.pagination.total}
-          title="search results"
-          path="search"
-          className="search__banner"
-        />
-        <Blogs blogs={blogs.data} pagination={blogs.meta.pagination} />
-      </Container>
-      <Footer />
-    </div>
-  );
+interface SearchParams {
+  con?: string;
+  des?: string;
+  q?: string;
+  cat?: string;
+  page?: string;
 }
 
-export default page;
+const PAGE_SIZE = 5;
+
+async function SearchPage({ searchParams }: { searchParams: SearchParams }) {
+  const { con, des, q, cat, page = "1" } = searchParams;
+
+  const filter = buildFilter({ con, des, q, cat });
+  const query = buildQuery(filter, parseInt(page));
+
+  try {
+    const blogs = await getStrapiData("blogs", query);
+
+    if (blogs.data.length === 0 && parseInt(page) > 1) {
+      notFound();
+    }
+
+    return (
+      <div className="search">
+        <Navbar />
+        <Container maxWidth="lg">
+          <Banner
+            resultCount={blogs.meta.pagination.total}
+            title="Search Results"
+            path="search"
+            className="search__banner"
+          />
+          <Blogs blogs={blogs.data} pagination={blogs.meta.pagination} />
+        </Container>
+        <Footer bg="bg2" />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching search results:", error);
+    notFound();
+  }
+}
+
+function buildFilter({
+  con,
+  des,
+  q,
+  cat,
+}: Omit<SearchParams, "page">): object[] {
+  const filter: object[] = [];
+  if (q) filter.push({ title: { $containsi: q } });
+  if (con) filter.push({ continent: { slug: { $eq: con } } });
+  if (des) filter.push({ destination: { slug: { $eq: des } } });
+  if (cat) filter.push({ categories: { slug: { $eq: cat } } });
+  return filter;
+}
+
+function buildQuery(filter: object[], page: number): string {
+  return qs.stringify({
+    populate: ["images.landscape"],
+    fields: ["title", "slug", "description", "createdAt", "readTime"],
+    filters: { $and: filter },
+    pagination: {
+      page,
+      pageSize: PAGE_SIZE,
+    },
+  });
+}
+
+export default SearchPage;

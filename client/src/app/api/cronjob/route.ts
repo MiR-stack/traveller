54571da7b2b72109a525e5ba48fb814fe63590ca
habibmir808 <habibmir808@gmail.com>
2025-01-17 18:21:41 +0600

    remove vercel cronjob

--- client/src/app/api/cronjob/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
-
-export async function GET(request: Request) {
-  const url = process.env.BASE_URL || "https://api.earthheavens.com";
-
-  const { status } = await fetch(url, { cache: "no-cache" });
-
-  return new Response(`status: ${status}`);
-}

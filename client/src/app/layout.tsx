2c0801e687ca62a7853db92c5382d83d985d6554
MiR-stack <habibmir808@gmail.com>
2025-08-17 15:58:40 +0600

    add adsence

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -67,6 +67,9 @@ async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>
+        <meta name="google-adsense-account" content="ca-pub-9802622065700000" />
+      </title>
       <body
         className={`${roboto.variable} ${roboto_Condensed.variable} ${roboto_Slab.variable} ${rock_salt.variable} ${oswald.variable} ${wellfleet.variable}`}
       >
